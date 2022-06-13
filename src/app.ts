a5db2937180d5e1e574f3f11055cad7627aa08f8
hosein fathi <hfathi711@gmail.com>
2022-06-13 11:41:55 +0430

    added book seller challenge

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import evenIndex from "./even-index/EvenIndex.js";
 import findMissingLetter from "./find-missing-letter/FindMissingLetter.js";
 import detectOddOrEvenArray from "./odd-or-even-array/OddOrEvenArray.js";
 import makeSpinWords from "./spin-words/SpinWords.js";
+import { BookSeller } from "./bookseller/Bookseller.js";
 
 const pangramSentence = "The quick brown fox jumps over the lazy dog";
 const isPangram = detectPangram(pangramSentence);
@@ -17,16 +18,17 @@ console.log(
 
 const arrayForEvenIndex = [1, 100, 50, -51, 1, 1];
 const arrayEvenIndex = evenIndex(arrayForEvenIndex);
-console.log(
-  `"even index of ${arrayForEvenIndex} is ${arrayEvenIndex}"`
-);
+console.log(`"even index of ${arrayForEvenIndex} is ${arrayEvenIndex}"`);
 
-const missingLetter = findMissingLetter(['O','Q','R','S']);
-console.log("Missing letter is",missingLetter);
+const missingLetter = findMissingLetter(["O", "Q", "R", "S"]);
+console.log("Missing letter is", missingLetter);
 
 const oddOrEvenArray = detectOddOrEvenArray([]);
-console.log("array is",oddOrEvenArray);
+console.log("array is", oddOrEvenArray);
 
 const spinWords = makeSpinWords("Hey fellow warriors");
-console.log("spinWords",spinWords);
+console.log("spinWords", spinWords);
 
+const bookArts: string[] = [];
+const booKCat = ["R", "Z", "S", "a", "m"];
+console.log("book seller result", BookSeller.stockList(bookArts, booKCat));
--- src/bookseller/Bookseller.ts
+++ src/bookseller/Bookseller.ts
@@ -1,24 +1,88 @@
-type IBookCatData = {
+type IBooksCatData = {
   cat: string;
   amount: number;
 }[];
 
+type IBookData = {
+  cat: string;
+  amount: number;
+};
+
 export class BookSeller {
-  public static stockList = (listOfArt: string[], listOfCat: string[]) => {
-    let bookCatObj: IBookCatData = [];
-    listOfArt.forEach((art) => {
-      bookCatObj.push({
-        amount: parseInt(art.split(" ")[1]),
-        cat: art.split(" ")[0],
-      });
-    });
-    const bookSeller = new BookSeller(bookCatObj);
-    console.log("bookSeller" ,bookSeller);
-    
+  private _booksCatData: IBooksCatData = [];
+
+  public static stockList = (
+    listOfArt: string[],
+    listOfCat: string[]
+  ): string => {
+    if (!listOfArt.length || !listOfCat.length) {
+      return "";
+    }
+    const bookSeller = new BookSeller(listOfArt);
+    return bookSeller.findCats(listOfCat);
   };
 
-  /**
-   *
-   */
-  constructor(private _bookCatData: IBookCatData) {}
+  constructor(books: string[]) {
+    let booksDataTemp: IBooksCatData = [];
+    books.forEach((book) => {
+      let bookData = this.convertBookStringToDataObj(book, booksDataTemp);
+      if (bookData) {
+        booksDataTemp.push(bookData);
+      }
+    });
+    this.booksCatData = booksDataTemp;
+  }
+
+  private convertBookStringToDataObj(
+    book: string,
+    booksData: IBooksCatData
+  ): IBookData | undefined {
+    const bookData = book.split(" ");
+    let bookDataTmp: IBookData | undefined = undefined;
+
+    // checking for valid book string
+    if (bookData.length === 2 && parseInt(bookData[1]) !== NaN) {
+      bookDataTmp = {
+        amount: parseInt(bookData[1]),
+        cat: bookData[0][0].toLowerCase(),
+      };
+      //finding same cat
+      let sameBookIndex = booksData.findIndex(
+        (book) => book.cat === bookDataTmp?.cat
+      );
+      // adding amount to same cat found in booksData array
+      if (sameBookIndex !== -1) {
+        booksData[sameBookIndex].amount += bookDataTmp.amount;
+        bookDataTmp = undefined;
+      }
+    } else {
+      throw new Error(
+        'Books List is not correct pls provide string array of "BOOK 20" '
+      );
+    }
+    return bookDataTmp;
+  }
+
+  private findCats(listOfCat: string[]): string {
+    let result = "";
+    listOfCat.forEach((cat, index, arr) => {
+      let bookCat = this.booksCatData.filter(
+        (book) => book.cat === cat.toLowerCase()
+      )[0];
+      result += `(${cat} : ${bookCat ? bookCat.amount : 0})`;
+      if (index !== arr.length - 1) {
+        result += " - ";
+      }
+    });
+
+    return result;
+  }
+
+  private set booksCatData(booksData: IBooksCatData) {
+    this._booksCatData = booksData;
+  }
+
+  private get booksCatData(): IBooksCatData {
+    return this._booksCatData;
+  }
 }
